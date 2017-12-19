56236bf24493da4ff5e24db70579f05b90ad560d
Andrea Falconi <andrea.falconi@gmail.com>
2017-12-19 11:30:48 +0100

    set up initial rendering ms proj structure.

---
+++ components/rnd-ms/src/main/java/rndms/core/Main.java
@@ -0,0 +1,9 @@
+package rndms.core;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("I'm useless at the mo...");
+    }
+
+}
---
+++ components/rnd-ms/src/test/java/rndms/core/MainTest.java
@@ -0,0 +1,10 @@
+package rndms.core;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void placeholder() {}
+
+}
